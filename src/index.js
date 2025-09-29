a0482145b05f12019cf6d4bdc5dd14de2e36de67
BlineraMoberg <bm222mr@student.lnu.se>
2025-09-29 20:39:31 +0200

    Need to wright more codelines. cus it need to be 200 minimum!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { Activity } from './Activity.js'
 export { Child } from './Child.js'
 export { DaySchedule } from './DaySchedule.js'
-export { WEEKDAY_COLORS, getColorsForDate } from './weekdayColors.js'
+export { WEEKDAY_COLORS, getColorForDate } from './weekdayColors.js'
